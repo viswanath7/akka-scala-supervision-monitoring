33e851b176167a969811a819852ba3ffc91f2a3d
Jayachandran, Viswanath <Viswanath.Jayachandran@aegon.nl>
2017-07-12 13:12:48 +0200

    Added documentation for unit tests

--- src/test/scala/com/example/akka/application/JulietSpec.scala
+++ src/test/scala/com/example/akka/application/JulietSpec.scala
@@ -4,13 +4,28 @@ import akka.actor.{ActorSystem, Props}
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import com.example.akka.application.Romeo.Tragic
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, MustMatchers}
+import org.slf4j.LoggerFactory
 
 
+/**
+  * Unit test for Juliet Actor
+  *
+  * The TestKit contains an actor named 'testActor' which is the entry point for messages to be examined
+  * with the various expectMsg() assertions.
+  *
+  * When mixing in the trait ImplicitSender this test actor is implicitly used as sender reference
+  * when dispatching messages from the test procedure.
+  *
+  */
 class JulietSpec extends TestKit(ActorSystem("test-system")) with ImplicitSender
   with FlatSpecLike with BeforeAndAfterAll with MustMatchers {
 
+  val logger = LoggerFactory getLogger this.getClass.getSimpleName
+
   override def afterAll: Unit = {
-    TestKit.shutdownActorSystem(system)
+    logger debug "Shutting down the actor system once all the tests have been completed " +
+      "so that all actors—including the test actor—are stopped. ..."
+    TestKit shutdownActorSystem system
   }
 
   "Juliet watching Romeo" should "kill herself when Romeo is terminated" in {
--- src/test/scala/com/example/akka/application/RomeoSpec.scala
+++ src/test/scala/com/example/akka/application/RomeoSpec.scala
@@ -4,13 +4,28 @@ import akka.actor.ActorSystem
 import akka.testkit.{ImplicitSender, TestKit, TestProbe}
 import com.example.akka.application.Romeo.Tragic
 import org.scalatest.{BeforeAndAfterAll, FlatSpecLike, MustMatchers}
+import org.slf4j.LoggerFactory
 
-
+/**
+  * Unit test for Romeo Actor
+  *
+  * The TestKit contains an actor named 'testActor' which is the entry point for messages to be examined
+  * with the various expectMsg() assertions.
+  *
+  * When mixing in the trait ImplicitSender this test actor is implicitly used as sender reference
+  * when dispatching messages from the test procedure.
+  *
+  */
 class RomeoSpec extends TestKit(ActorSystem("test-system")) with ImplicitSender
   with FlatSpecLike with BeforeAndAfterAll with MustMatchers {
 
+  val logger = LoggerFactory getLogger this.getClass.getSimpleName
+
+
   override def afterAll: Unit = {
-    TestKit.shutdownActorSystem(system)
+    logger debug "Shutting down the actor system once all the tests have been completed " +
+      "so that all actors—including the test actor—are stopped. ..."
+    TestKit shutdownActorSystem system
   }
 
   "Romeo actor" must "stop oneself upon receiving Tragic message" in {
